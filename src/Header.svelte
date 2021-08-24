fd41933d6726e903da81633b08e5f0fb68fe9737
Andre Viallon <viallon.andre@gmail.com>
2021-08-24 20:49:42 +0200

    chore: create basic  header

---
+++ src/Header.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  let logo = 'images/logo.svg';
+  export let isShowingSlideShow = false;
+
+  console.log('isShowingSlideShow', isShowingSlideShow);
+  
+</script>
+
+<div class="header">
+  <img src={logo} alt="logo"/>
+  {#if isShowingSlideShow}
+    <a href="/" class="exit">Stop Slideshow</a>  
+    {:else}<a href="/" class="exit">Start Slideshow</a>
+  {/if}
+</div>
+
+<style>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    margin: 2rem;
+  }
+  img {
+    width: 8rem;
+  }
+  .exit {
+    text-transform: uppercase;
+  }
+</style>
