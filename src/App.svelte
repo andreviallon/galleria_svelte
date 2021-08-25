9a0821722996a7dbba6680ab2a8bc4966a0077b1
Andre Viallon <viallon.andre@gmail.com>
2021-08-25 20:02:08 +0200

    chore: show gallery component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import Header from './Header.svelte';
+	import Gallery from './Gallery.svelte';
 	let isShowingSlideShow = false;
 </script>
 
 <main>
 	<Header isShowingSlideShow={isShowingSlideShow}></Header>
+	<Gallery />
 </main>
